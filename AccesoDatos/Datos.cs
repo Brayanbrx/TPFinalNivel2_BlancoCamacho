8702de2934baf54954a4a56875fa1471b42fee8c
Brayanbrx <brayanbrx1@gmail.com>
2023-09-11 16:42:40 -0400

    Agregar archivos de proyecto.

---
+++ AccesoDatos/Datos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace AccesoDatos
+{
+    public class Datos
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public SqlDataReader Reader 
+        {
+            get { return reader; }         
+        }
+
+        public Datos()
+        {
+            connection = new SqlConnection("server =.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            command = new SqlCommand();
+        }
+
+        public void setConsulta(string consulta)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = consulta;
+        }
+        public void ejecutarLectura()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setParametro(string nombre, object valor)
+        {
+            command.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void cerrarConexion()
+        {
+            if (reader != null)
+                reader.Close();
+            connection.Close();
+        }
+
+
+
+    }
+}
---
+++ Negocio/ArticuloNegocio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> Listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+
+            try
+            {
+
+                return lista;   
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+    }
+}
